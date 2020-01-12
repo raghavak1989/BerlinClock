31ac4b94d3361be48b9442f114adaea05e29c615
raghavak1989 <raghava40@gmail.com>
2020-01-12 22:08:20 +0530

    Add files via upload

---
+++ BerlinClock/BerlinClock/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BerlinClock
+{
+	class Program
+	{
+		// Y - Indicates Yellow light in the output
+		// R - Indicates red light in hours and in minutes it indicates past 15,30, and 45 minutes
+		// O - Indicates that the light is off and not lit for the time given
+
+		public class BerlinClock
+		{
+			public String convertTime(String aTime)
+			{
+				int[] parts = aTime.Split(':').Select(Int32.Parse).ToArray();
+
+				StringBuilder time = new StringBuilder();
+
+				return time.AppendLine(getSeconds(parts[2]))
+						.AppendLine(getTopHours(parts[0]))
+						.AppendLine(getBottomHours(parts[0]))
+						.AppendLine(getTopMinutes(parts[1]))
+						.AppendLine(getBottomMinutes(parts[1])).ToString();
+
+			}
+			private String getSeconds(int number)
+			{
+				if (number % 2 == 0)
+					return "Y";
+				else
+					return "O";
+			}
+			private String getTopHours(int number)
+			{
+				return getOnOff(4, getTopNumberOfOnSigns(number));
+			}
+			private String getBottomHours(int number)
+			{
+				return getOnOff(4, number % 5);
+			}
+			private String getTopMinutes(int number)
+			{
+				return getOnOff(11, getTopNumberOfOnSigns(number), "Y").Replace("YYY", "YYR");
+			}
+			private String getBottomMinutes(int number)
+			{
+				return getOnOff(4, number % 5, "Y");
+			}
+			private String getOnOff(int lamps, int onSigns)
+			{
+				return getOnOff(lamps, onSigns, "R");
+			}
+			private String getOnOff(int lamps, int onSigns, String onSign)
+			{
+				string output = "";
+				for (int i = 0; i < onSigns; i++)
+				{
+					output += onSign;
+				}
+				for (int i = 0; i < (lamps - onSigns); i++)
+				{
+					output += "O";
+				}
+				return output;
+			}
+			private int getTopNumberOfOnSigns(int number)
+			{
+				return (number - (number % 5)) / 5;
+			}
+			static void Main(string[] args)
+			{
+				BerlinClock berlinClock = new BerlinClock();
+				Console.WriteLine(berlinClock.convertTime("13:15:40"));
+				Console.ReadLine();
+
+			}
+		}
+	}
+}
---
+++ BerlinClock/BerlinClock/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BerlinClock")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Radiometer Medical")]
+[assembly: AssemblyProduct("BerlinClock")]
+[assembly: AssemblyCopyright("Copyright © Radiometer Medical 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ffdbbd6c-6f0e-4d9b-bf10-77e345410dd2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
